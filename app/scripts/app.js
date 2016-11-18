985bcf7993be5b4c573b2e26e86866bf3f5311a1
Davey <dwaite498@gmail.com>
2016-11-18 12:05:24 -0500

    add landingCtrl, add some angular attempts

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+var blocChat = angular.module("blocChat", [])
+.controller('blocChat', function landingCtrl() {
+    
+})
+-------------------------
+
+(function() {
+    function blocChat() {
+        
+    }
+    
+    angular.module('blocChat')
+    .controller('blocChat', [$scope/blocChat, 'ui.router', 'firebase'])
+})();
+
+//What is the difference between these two formats, is the scope dependency need to be module name or $scope?
